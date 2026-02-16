d68b837587e4243cdcf757314dc122749e8b272f
Ivan Zinchenko <i@zinch.me>
2026-02-16 21:07:43 +0300

    wip

--- is-lab3-jee/src/main/java/me/zinch/is/islab3/server/datasource/GlassfishDruidDataSource.java
+++ is-lab3-jee/src/main/java/me/zinch/is/islab3/server/datasource/GlassfishDruidDataSource.java
@@ -22,4 +22,44 @@ public class GlassfishDruidDataSource extends DruidDataSource {
     public void setBreakAfterAcquireFailure(String value) {
         super.setBreakAfterAcquireFailure(Boolean.parseBoolean(value));
     }
+
+    public void setTestOnBorrow(String value) {
+        super.setTestOnBorrow(Boolean.parseBoolean(value));
+    }
+
+    public void setTestWhileIdle(String value) {
+        super.setTestWhileIdle(Boolean.parseBoolean(value));
+    }
+
+    public void setTestOnReturn(String value) {
+        super.setTestOnReturn(Boolean.parseBoolean(value));
+    }
+
+    public void setKeepAlive(String value) {
+        super.setKeepAlive(Boolean.parseBoolean(value));
+    }
+
+    public void setValidationQueryTimeout(String seconds) {
+        super.setValidationQueryTimeout(Integer.parseInt(seconds));
+    }
+
+    public void setKeepAliveBetweenTimeMillis(String millis) {
+        super.setKeepAliveBetweenTimeMillis(Long.parseLong(millis));
+    }
+
+    public void setTimeBetweenEvictionRunsMillis(String millis) {
+        super.setTimeBetweenEvictionRunsMillis(Long.parseLong(millis));
+    }
+
+    public void setMinEvictableIdleTimeMillis(String millis) {
+        super.setMinEvictableIdleTimeMillis(Long.parseLong(millis));
+    }
+
+    public void setMaxWait(String millis) {
+        super.setMaxWait(Long.parseLong(millis));
+    }
+
+    public void setLoginTimeout(String seconds) {
+        super.setLoginTimeout(Integer.parseInt(seconds));
+    }
 }
